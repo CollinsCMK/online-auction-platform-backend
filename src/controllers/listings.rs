8505c20aec48a2ca12ceb2fb58592e0dff2f5d71
CollinsCMK <collinscmkdev@gmail.com>
2025-02-19 09:49:45 +0300

    CMK: Updated listings table

--- src/controllers/listings.rs
+++ src/controllers/listings.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use actix_web::{delete, get, post, put, web};
 use chrono::{NaiveDateTime, Utc};
 use rust_decimal::Decimal;
-use sea_orm::{ActiveModelTrait, EntityTrait, QueryFilter, Set, ColumnTrait};
+use sea_orm::{ActiveModelTrait, ColumnTrait, EntityTrait, FromQueryResult, QueryFilter, QuerySelect, Set};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
@@ -13,10 +13,9 @@ use crate::utils::{api_response::ApiResponse, app_state::AppState, json_response
 struct ListingData {
     title: String,
     description: Option<String>,
-    start_time: NaiveDateTime,
-    end_time: NaiveDateTime,
     base_price: String,
     available_volume: Option<i32>,
+    auction_id: i32,
 }
 
 impl ListingData {
@@ -25,24 +24,12 @@ impl ListingData {
             return Err("Title is required".to_string());
         }
 
-        if self.start_time.to_string().is_empty() {
-            return Err("Start time is required".to_string());
-        }
-
-        if self.end_time.to_string().is_empty() {
-            return Err("End time is required".to_string());
-        }
-        
-        if self.start_time > self.end_time {
-            return Err("Start time must be before end time".to_string());
-        }
-
-        if self.end_time <= self.start_time {
-            return Err("End time must be after start time".to_string());
-        }
-
         if self.base_price.to_string().is_empty() {
             return Err("Base Price is required".to_string());
+        }
+
+        if self.auction_id.to_string().is_empty() {
+            return Err("Auction ID is required".to_string());
         }
 
         Ok(())
@@ -62,25 +49,10 @@ pub async fn create_listing(
         )));
     }
 
-    let auction_model = entity::auctions::ActiveModel {
-        start_time: Set(listing_data.start_time.clone()),
-        end_time: Set(listing_data.end_time.clone()),
-        ..Default::default()
-    }
-        .insert(&app_state.db)
-        .await
-        .map_err(|err| {
-            ApiResponse::new(500, response(
-                json!({
-                    "error": err.to_string()
-                })
-            ))
-        })?;
-
     entity::listings::ActiveModel {
         title: Set(listing_data.title.clone()),
         description: Set(listing_data.description.clone()),
-        auction_id: Set(auction_model.id),
+        auction_id: Set(listing_data.auction_id.clone()),
         base_price: Set(Decimal::from_str(&listing_data.base_price.clone()).expect("A decimal number")),
         available_volume: Set(listing_data.available_volume.unwrap_or(1)),
         ..Default::default()
@@ -130,6 +102,7 @@ pub async fn update_listing(
 
     let mut update_listing_model: entity::listings::ActiveModel = listing_model.to_owned().into();
     update_listing_model.title = Set(listing_data.title.clone());
+    update_listing_model.auction_id = Set(listing_data.auction_id.clone());
     update_listing_model.description = Set(listing_data.description.clone());
     update_listing_model.base_price = Set(Decimal::from_str(&listing_data.base_price.clone()).expect("A decimal number"));
     update_listing_model.available_volume = Set(listing_data.available_volume.unwrap_or(1));
@@ -152,12 +125,41 @@ pub async fn update_listing(
     )))
 }
 
-#[get("/listings/get")]
+#[derive(Debug, FromQueryResult)]
+struct  AuctionResult {
+    name: String,
+    start_time: NaiveDateTime,
+    end_time: NaiveDateTime,
+    id: i32,
+    title: String,
+    description: Option<String>,
+    base_price: Decimal,
+    available_volume: i32,
+    updated_at: NaiveDateTime,
+}
+
+#[get("/listings/get/{id}")]
 pub async fn get_all_listings(
+    path: web::Path<i32>,
     app_state: web::Data<AppState>,
 ) -> Result<ApiResponse, ApiResponse> {
+    let auction_id = path.into_inner();
+
     let listing_model = entity::listings::Entity::find()
+        .inner_join(entity::auctions::Entity)
+        .filter(entity::auctions::Column::Id.eq(auction_id))
         .filter(entity::listings::Column::DeletedAt.is_null())
+        .select_only()
+        .column(entity::auctions::Column::Name)
+        .column(entity::auctions::Column::StartTime)
+        .column(entity::auctions::Column::EndTime)
+        .column(entity::listings::Column::Id)
+        .column(entity::listings::Column::Title)
+        .column(entity::listings::Column::Description)
+        .column(entity::listings::Column::BasePrice)
+        .column(entity::listings::Column::AvailableVolume)
+        .column(entity::listings::Column::UpdatedAt)
+        .into_model::<AuctionResult>()
         .all(&app_state.db)
         .await
         .map_err(|err| {
@@ -167,7 +169,7 @@ pub async fn get_all_listings(
                 })
             ))
         })?
-        .iter()
+        .into_iter()
         .map(|row| {
             json!({
                 "id": row.id,
@@ -175,6 +177,9 @@ pub async fn get_all_listings(
                 "description": row.description,
                 "base_price": row.base_price,
                 "available_volume": row.available_volume,
+                "auction_name": row.name,
+                "start_time": row.start_time,
+                "end_time": row.end_time,
                 "updated_at": row.updated_at,
             })
         })
